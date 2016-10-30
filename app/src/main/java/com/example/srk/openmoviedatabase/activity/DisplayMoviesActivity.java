f9e2e3e6a3a6a6186ec730520fea5b4a9851acbf
Raghu <sai.raghu.kiran@gmail.com>
2016-10-30 12:33:07 -0500

    Initial commit.

---
+++ app/src/main/java/com/example/srk/openmoviedatabase/activity/DisplayMoviesActivity.java
@@ -0,0 +1,15 @@
+package com.example.srk.openmoviedatabase.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.srk.openmoviedatabase.R;
+
+public class DisplayMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_movies);
+    }
+}
